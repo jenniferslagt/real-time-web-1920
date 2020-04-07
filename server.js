87972d14032384371f0ca77b5fb15890a9e30c27
jenniferslagt <45489420+jenniferslagt@users.noreply.github.com>
2020-04-07 14:28:30 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-const io = require("socket.io")(3000);
+const express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function () {
+    console.log('listening on port 3000');
+});
+
+// set a static folder 
+// app.use(express.static(path.join(__dirname, "public")));
+
 
 const users = {}
+
 
 io.on(("connection"), function (socket) {
     console.log('new user ');
