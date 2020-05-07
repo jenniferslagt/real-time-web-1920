73088b402bd2783120771882a814c0a45febebcf
jenniferslagt <45489420+jenniferslagt@users.noreply.github.com>
2020-05-07 09:45:09 +0200

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -14,6 +14,7 @@ const map = new H.Map(
         }
     }
 );
+
 const mapEvent = new H.mapevents.MapEvents(map);
 const behavior = new H.mapevents.Behavior(mapEvent);
 
@@ -21,26 +22,21 @@ const socket = io("http://localhost:3000");
 
 // Draw a marker when the users "taps"
 socket.on("marker", data => {
-    const marker = new H.map.Marker(data, {
-        strokeColor: "red"
-    });
-    map.addObject(marker);
+
+    addMarkerToMap(data);
+    addPolylineToMap(map);
 });
 
-// Add an undo button
-// var body = document.querySelector("body");
-// var undoButton = document.createElement('button');
-// undoButton.innerText = "reset"
-// body.appendChild(undoButton);
-
-// undoButton.addEventListener("click", event => {
-//     console.log('CLICK')
-//     // markerLocation.pop()
-//     location.reload();
-// })
-
-
+// Make a empty array with the locations of the markers
 let markerLocation = [];
+let calculatedTotalDistance = 0;
+
+// Adding events to buttons
+const newBtn = document.getElementById("new-btn")
+newBtn.addEventListener("click", event => {
+    markerLocation = [];
+})
+
 let color = 'red'
 
 const redBtn = document.getElementById("red-btn");
@@ -49,7 +45,7 @@ redBtn.addEventListener("click", event => {
 });
 const pinkBtn = document.getElementById("pink-btn");
 pinkBtn.addEventListener("click", event => {
-    color = 'pink'
+    color = '#c96bff'
 });
 const blueBtn = document.getElementById("blue-btn");
 blueBtn.addEventListener("click", event => {
@@ -57,7 +53,7 @@ blueBtn.addEventListener("click", event => {
 });
 const greenBtn = document.getElementById("green-btn");
 greenBtn.addEventListener("click", event => {
-    color = 'green'
+    color = '#03fc41'
 });
 const yellowBtn = document.getElementById("yellow-btn");
 yellowBtn.addEventListener("click", event => {
@@ -68,16 +64,25 @@ blackBtn.addEventListener("click", event => {
     color = 'black'
 });
 
-
+// Adding a marker on the place where the users clicks
 map.addEventListener("tap", event => {
     const position = map.screenToGeo(
         event.currentPointer.viewportX,
         event.currentPointer.viewportY
     );
+    console.log('markerlocaitons', markerLocation)
 
+    addMarkerToMap(position);
+    addPolylineToMap(map);
+    calculateDistance();
+    socket.emit("marker", position);
+});
+
+
+// Add a marker to the map and the markerLocations array
+function addMarkerToMap(position) {
     const marker = new H.map.Marker(position);
     map.addObject(marker);
-    socket.emit("marker", position);
 
     let latitude = position.lat;
     let longitude = position.lng;
@@ -85,45 +90,59 @@ map.addEventListener("tap", event => {
         latitude,
         longitude
     });
+}
 
-    // Add lines between the marker
-    function addPolylineToMap(map) {
-        const lineString = new H.geo.LineString();
-        markerLocation.forEach(location => {
-            lineString.pushPoint({
-                lat: location.latitude,
-                lng: location.longitude
-            });
-        })
+function calculateDistance() {
+    const markerLength = markerLocation.length;
+    console.log(markerLength)
+    markerLocation.map((distance, index) => {
+        // console.log('check deze', distance, index)
+        if (index < markerLength - 1) {
+            const currentDistance = distance;
+            const nextDistance = markerLocation[index + 1];
+            calculatedTotalDistance += getDistanceFromLatLonInKm(currentDistance.latitude, currentDistance.longitude, nextDistance.latitude, nextDistance.longitude);
+            console.log(calculatedTotalDistance);
+        }
+    })
+}
 
-        map.addObject(new H.map.Polyline(
-            lineString, {
-                style: {
-                    lineWidth: 4,
-                    strokeColor: color
-                }
+
+// Add lines between the marker
+function addPolylineToMap(map) {
+    const lineString = new H.geo.LineString();
+    markerLocation.forEach(location => {
+        lineString.pushPoint({
+            lat: location.latitude,
+            lng: location.longitude
+        });
+    })
+
+    map.addObject(new H.map.Polyline(
+        lineString, {
+            style: {
+                lineWidth: 4,
+                strokeColor: color
             }
-        ));
-    }
-    addPolylineToMap(map);
+        }
+    ));
+}
 
-});
+console.log('markerarray', markerLocation)
+console.log('afstand in km', getDistanceFromLatLonInKm(37.7397, -121.4252, 37.9577, -121.2908));
 
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return Math.round(Number(d.toFixed(1)) + 1)
+}
 
-
-
-
-
-// Calculate the distance between the polylines
-const tracyMarker = new H.map.Marker({
-    lat: 37.7397,
-    lng: -121.4252
-});
-
-const stocktonMarker = new H.map.Marker({
-    lat: 37.9577,
-    lng: -121.2908
-});
-
-const distance = tracyMarker.getPosition().distance(stocktonMarker.getPosition())
-console.log("afstand in meters", distance)
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
