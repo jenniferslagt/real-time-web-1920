91ff699108ba7063d42d14e7ed38d55e7460320f
jenniferslagt <45489420+jenniferslagt@users.noreply.github.com>
2020-05-15 11:07:48 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -5,52 +5,84 @@ var io = require('socket.io')(http);
 require('dotenv').config();
 
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-const randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+const routeRooms = ["walking route", "cycle route", "car route"];
+
+// Store the data of the users
 const userList = [];
+const user = {};
+const markerList = [];
 
-const markers = [];
-const user = [];
-const users = [];
-
-// Make a event when a user is connected
-io.on("connection", socket => {
-
-    // the event name is "message" en het output is hello world
-    // socket.emit("message", "hello world")
-
-    // get the event with the data from the client on the server
-    // socket.on("test-message", message => {
-    //     console.log("This is the test message", message)
-    // })
+// Generate a random color for each user
+const randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 
 
-    for (let i = 0; i < markers.length; i++) {
-        socket.emit("draw-route", markers[i]);
-    }
+// with namespaces you change the path (or route), to separate the connections 
+// to create a namespace and acces, you have to use the of method
+io.of("/drawroute").on("connection", socket => {
 
-    socket.on("draw-route", data => {
-        markers.push(data);
+    socket.emit("welcome", "hello and welcome to the games area")
+
+    socket.on("joinRoom", room => {
+        // before joining a room, check if the game does exist on the list 
+        // the includes method takes any string and checks if it exists
+        if (routeRooms.includes(room)) {
+            socket.join(room)
+
+            // Use the io library, this is broadcasting to the whole thing (instead of the only socket)
+            // Acces the namespace and then the room, this is must when you want to acces the socket
+            io.of("/drawroute").in(room).emit("newUser", "new player has joined the" + room)
+            io.of("/drawroute").in(room).emit("rooms", routeRooms)
+            io.of("/drawroute").in(room).emit("random-color", randomColor)
 
 
-        io.emit("draw-route", data);
-    });
+            // Share the route with other sockets
+            for (let i = 0; i < markerList.length; i++) {
+                socket.emit("draw-route", markerList[i]);
+            }
 
-    user.push(['id:', socket.id, 'color:', 'red', ]);
-    console.log(user)
-    const obj = Object.fromEntries(user);
-    console.log("object", obj)
-    users.push
-});
+            socket.on("draw-route", data => {
+                markerList.push(data);
+                io.of("/drawroute").in(room).emit("draw-route", data);
+                user.markerList = markerList;
+                console.log("user list", userList)
+                io.of("/drawroute").in(room).emit("user-list", userList);
+
+            });
+
+            // Add data to the userList to store all the data of the users
+            user.id = socket.id;
+            user.color = randomColor;
+            console.log("user", user)
+            userList.push(user);
+
+            return socket.emit("succes", "you have succesfully joines this room" + room)
+        } else {
+            return socket.emit("err", "ERROR no room named" + room)
+        }
+
+    })
+
+    // socket.disconnect();
+
+})
+
 
 
 http.on('listening', function () {
-    console.log('ok, server is running');
+    console.log('yay, server is running');
 });
 
-http.listen(process.env.PORT || 5555, function () {
-    console.log("listening on *:5555");
+http.listen(process.env.PORT || 3333, function () {
+    console.log("listening on *:3333");
 });
+
+
+// Here some import notes which can be helpfull:
+// *  Socket.io picks the name and creates actually a room for you
+// *  The in method sends an event to all the user including the "new" socket
+// *  The of method sends an event ot all the users excluding the "new" socket
